a4ce6c843759777e18d7ab12b39bb9370bc86723
gabriel <franklingabriel158@gmail.com>
2022-03-29 15:43:13 -0300

    iniciando techAssistance

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+  
+  private config(): void {
+    const acessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Acess-Control-Allow-Origin', '*');
+      res.header('Acess-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+      res.header('Acess-Control-Allow-Headers', '*');
+      next();
+    };
+    this.app.use(acessControl);
+    this.app.use(express.json());
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
+  }
+}
+
+export { App };
